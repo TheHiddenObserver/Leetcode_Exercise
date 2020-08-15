093c6f53a6cf50d201eadea95087652b26f72302
TheHiddenObserver <51812297+TheHiddenObserver@users.noreply.github.com>
2020-08-15 18:59:33 +0800

    Create 1_两数之和.py

---
+++ 哈希表/1_两数之和.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i, n in enumerate(nums):
+            if target - n in dic:
+                return [i,dic[target-n]]
+            dic[n] = i
+        return [-1]
